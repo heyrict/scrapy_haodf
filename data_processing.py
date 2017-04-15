349c978e4ece3cc63d56efb67332684a88a6f199
heyrict <xiezh0831@126.com>
2017-04-15 16:18:20 +0800

    add functions to process so_called 'stacked_data'

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import pandas as pd, numpy as np
+
 def flip_dict(dict_to_flip):
     return dict([i[::-1] for i in dict_to_flip.items()])
 def split_wrd(string,sep,rep=None):
@@ -31,3 +33,38 @@ def flip_dict_full(dict_to_flip):
             tp[i[0]] = tp[i[0]] + [i[1]] if type(tp[i[0]])==list else [tp[i[0]]] + [i[1]]
         else: tp[i[0]] = [i[1]]
     return tp
+def transfer_datatype(data):
+    if type(data) == list or not data: return data
+    else:
+        try: return eval(data)
+        except: return data
+        
+def stacked_series_flatten(ser):
+    ser = ser.map(transfer_datatype)
+    sser = ser[ser.map(type) != list]
+    lser = ser[ser.map(type) == list]
+    maxn = lser.map(len).max()
+    if np.isnan(maxn): return sser
+    for i in range(int(maxn)):
+        sser = sser.append(lser.map(lambda x: x[i] if len(x)>i else None),ignore_index=True)
+    return sser.dropna()
+def stacked_series_map(ser,mapfunc='count',label=None,ascending=False):
+    ser.name = ser.name if ser.name else 'ser'
+    df = pd.DataFrame(stacked_series_flatten(ser),columns=[ser.name])
+    df['result'] = 1
+    if type(mapfunc) == str:
+        if mapfunc not in ['count','sum']: raise ValueError('mapfunc param not supported, please input count or sum')
+        elif mapfunc == 'sum':
+            dfp = df.groupby(ser.name).sum().rename_axis({'result':'sum'},axis=1).sort_values('sum',ascending=ascending)
+        elif mapfunc == 'count':
+            dfp = df.groupby(ser.name).count().rename_axis({'result':'count'},axis=1).sort_values('count',ascending=ascending)
+    else:
+        dfp = df.groupby(ser.name).map(mapfunc).sort_values('result',ascending=ascending)
+    if type(label) == type(None): return dfp
+    else:
+        if type(label) in [dict,pd.Series]: return dfp.rename_axis(label)
+        elif type(label) == pd.DataFrame:
+            if label.shape[1]!=2:
+                raise ValueError('Length of DataFrame columns must be 2; %i detected'%label.shape[1])
+            return dfp.rename_axis(dict(label.values))
+        
